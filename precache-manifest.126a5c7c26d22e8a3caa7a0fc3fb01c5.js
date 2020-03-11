58797e837fcab442ffdfa5416630ce11ea402968
danielm2048 <danielmimoun98@gmail.com>
2020-03-12 00:30:32 +0200

    Updates

---
+++ precache-manifest.126a5c7c26d22e8a3caa7a0fc3fb01c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4649ba44f3ed619bd89333b9713959e8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0423ec4dddb1f0ceddda",
+    "url": "/Portfolio/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "818a4435316c6247a0ce",
+    "url": "/Portfolio/static/js/2.7f92c079.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/Portfolio/static/js/2.7f92c079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0423ec4dddb1f0ceddda",
+    "url": "/Portfolio/static/js/main.7b6fa0c1.chunk.js"
+  },
+  {
+    "revision": "654ea12aa83236a26bec",
+    "url": "/Portfolio/static/js/runtime-main.e1c49509.js"
+  }
+]);
